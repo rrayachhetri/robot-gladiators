322194fb9249e20fe15d1a956221a8625d155898
rrayachhetri <rrayachhetri@gmail.com>
2021-07-08 23:35:36 -0700

    if else if

--- assets/js/game.js
+++ assets/js/game.js
@@ -35,11 +35,18 @@ var enemyAttack = 12;
 var fight = function() {
      // Alert players that they are starting the round
     window.alert("Welcome to Robot Gladiators!");
+var promptfight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
+// if player chose to fight, then fight 
+if (promptfight === "fight" || promptfight === "FIGHT") {
+ 
+  
 
-  //Subtract the value of `playerAttack` from the value of `enemyHealth` and use that result to update the value in the `enemyHealth` variable
+
+// remove enemy's health by subtracting the amount set in the playerAttack variable 
    enemyHealth = enemyHealth - playerAttack;
   // Log a resulting message to the console so we know that it worked.
    console.log( playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining.");
+
   // check enemy's health
   if (enemyHealth <= 0) {
       window.alert(enemyName + " has died!");
@@ -47,7 +54,7 @@ var fight = function() {
   else {
       window.alert(enemyName + " still has " + enemyHealth + " health left.");
   }
-  // Subtract the value of `enemyAttack` from the value of `playerHealth` and use that result to update the value in the `playerHealth` variable.
+  // remove player's health by subtracting the amount set in the enemyAttack variable
    playerHealth = playerHealth - enemyAttack;
   // Log a resulting message to the console so we know that it worked.
    console.log(enemyName + " attacked " + playerName + ". " +  playerName + " now has " + playerHealth + " health remaining.");
@@ -58,7 +65,12 @@ var fight = function() {
  else {
     window.alert(playerName + " still has " + playerHealth + " health left.");
  }
-   
+   // if player choses to skip
+} else if (promptfight === "skip" || promptfight === "SKIP") {
+    window.alert(playerName + " has choosen to skip the fight!");
+} else {
+    window.alert("You need to choose a valid option. Try again!");
+}
 
 
 };
